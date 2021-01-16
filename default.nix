f889c8c728b312a8f40a848f9cad5b4b23b468f9
Kirill Kuznetsov <kirill.desirehd@gmail.com>
2021-01-16 19:33:08 +0300

    fix #1: Support Big Sur building

--- default.nix
+++ default.nix
@@ -3,23 +3,45 @@
 let
   sources = import ./nix/sources.nix;
   haskell-nix = import sources."haskell.nix" {};
-  iohk = (import sources.nixpkgs haskell-nix.nixpkgsArgs).pkgs.haskell-nix;
-  pkgs = import sources.nixpkgs { };
-  pkgsSet = (iohk.stackProject {
-    src = sources.purescript-native;
-    pkg-def-extras = [
-      (hackage: { hsc2hs = hackage.hsc2hs."0.68.4".revisions.default; })
+  nixpkgsArgs = haskell-nix.nixpkgsArgs // {
+    config = {};
+    overlays = haskell-nix.nixpkgsArgs.overlays ++ [
+      (
+        self: super: {
+          haskell-nix = super.haskell-nix // {
+            compiler = super.haskell-nix.compiler // {
+              ghc865 = super.haskell-nix.compiler.ghc865.overrideAttrs (
+                prev: {
+                  src = prev.src.overrideAttrs (
+                    prevSrc: {
+                      patches = prevSrc.patches ++ [
+                        ./patches/fix-ghc865.patch
+                      ];
+                    }
+                  );
+                }
+              );
+            };
+          };
+        }
+      )
     ];
-    modules = [
-      {
-        packages.purescript.components.library.build-tools = [pkgs.darwin.apple_sdk.frameworks.Cocoa];
-      }
-    ];
-    sha256map = {
-      "https://github.com/purescript/purescript"."9cad73ed8ea7df3011032ddbd2f3de5a0c08629c" = "0mlgww6j8nfdgy6x7l6z5in37r6qs11wnxf1cpgqaiyabadaik20";
-    };
-  });
-in pkgs.symlinkJoin {
+  };
+  iohk = (import sources.nixpkgs nixpkgsArgs).pkgs.haskell-nix;
+  pkgs = import sources.nixpkgs {};
+  pkgsSet = (
+    iohk.stackProject {
+      src = sources.purescript-native;
+      pkg-def-extras = [
+        (hackage: { hsc2hs = hackage.hsc2hs."0.68.4".revisions.default; })
+      ];
+      sha256map = {
+        "https://github.com/purescript/purescript"."9cad73ed8ea7df3011032ddbd2f3de5a0c08629c" = "0mlgww6j8nfdgy6x7l6z5in37r6qs11wnxf1cpgqaiyabadaik20";
+      };
+    }
+  );
+in
+pkgs.symlinkJoin {
   name = "purescript-native";
   paths = with pkgs; [
     go
